7157881265ff15c4bb9a32ed28fd1fecc94b483e
Jean Guillet <jean.guillet1@gmail.com>
2022-05-19 20:42:12 +0200

    Solving ex02

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,6 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+  validates :list_id, uniqueness: { scope: :movie_id }
+  validates :comment, { presence: true, length: { minimum: 6 } }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "json"
+require "open-uri"
+
+Movie.destroy_all
+
+URL = "https://tmdb.lewagon.com/movie/top_rated"
+IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original"
+
+response = JSON.parse(URI.open(URL).read)
+
+response["results"].each do |movie|
+  Movie.create({
+    title: movie['original_title'],
+    overview: movie['overview'],
+    poster_url: "#{IMAGE_BASE_URL}#{movie['backdrop_path']}",
+    rating: movie['vote_average']
+  })
+end
